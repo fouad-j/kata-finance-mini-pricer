3792b99e362de6064599df6635b8143cddfbf1fe
fouad-j <fouad.j@live.fr>
2017-04-15 00:20:59 +0200

    step1 without last part

--- src/pricer.js
+++ src/pricer.js
@@ -1,6 +1,14 @@
+import moment from 'moment';
 
+// TODO check validity of input
 let pricer = (futureDate, currentPrice, volatility) => {
-  return 50;
+  return calculPrice(moment(), futureDate, currentPrice, volatility);
+};
+
+let calculPrice = (currentDate, futureDate, currentPrice, volatility) => {
+  return currentDate.format('DD/MM/YYYY') !== futureDate.format('DD/MM/YYYY') ?
+    calculPrice(currentDate.add(1, 'd'), futureDate, currentPrice*(1+volatility/100), volatility) :
+    currentPrice;
 };
 
 export default pricer;
--- src/pricer.spec.js
+++ src/pricer.spec.js
@@ -2,7 +2,11 @@ import pricer from './pricer';
 import moment from 'moment';
 
 describe('Should test pricer', () => {
-  it('Should get Hello from pricer', () => {
-    expect(pricer(moment().format('DD/MM/YYYY'), 50, 100)).toEqual(50);
+  it('Should return the same price for the same day', () => {
+    expect(pricer(moment(), 50, 100)).toEqual(50);
+  });
+
+  it('Should return 100 for one day more', () => {
+    expect(pricer(moment().add(1, 'd'), 50, 100)).toEqual(100);
   });
 });
